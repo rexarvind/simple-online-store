76d24eb62d3ba47f9e5569695f68c639c843d978
Arvind Kumar <63487995+rexarvind@users.noreply.github.com>
2020-06-05 09:11:04 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,38 @@
+var products=[
+{id:'CT22062019',
+name:"Cine LUTs for Sony A7III, A7RIII0",
+category:'Table',
+date:'22 June 2019',
+image:'items/500.jpg',
+material:'Wood',
+price:2900,
+oldPrice:'Rs. 3200',
+seller:'Happy Workers',
+about:'Here you can tell something about this product. You can also write unique features specific to this product.'},
+
+{id:'CT22062019',
+name:"Sony Vegas Pro Max Ultimate Ultra",
+category:'Table',
+date:'22 June 2019',
+image:'items/528.jpg',
+material:'Wood',
+price:1200,
+oldPrice:'Rs. 3200',
+seller:'Happy Workers',
+about:'Here you can tell something about this product. You can also write unique features specific to this product.'},
+
+{id:'CT22062019',
+name:"Cine LUTs for Sony A7III, A7RIII",
+category:'Table',
+date:'22 June 2019',
+image:'items/528.jpg',
+material:'Wood',
+price:2900,
+oldPrice:'Rs. 3200',
+seller:'Happy Workers',
+about:'Here you can tell something about this product. You can also write unique features specific to this product.'},
+
+
+
+
+]
